4b1b55695ad00b066c10fef59c58a1e365d465bf
Ranjitprakash Sundaramurthi <ranjitprakash1986@gmail.com>
2023-01-13 14:44:57 -0800

    feat: add sentiment score plotting function docstring

---
+++ src/pysentimentanalyzer/sentiment_score_plot.py
@@ -0,0 +1,23 @@
+def sentiment_score_plot(df, col):
+    """
+    Creates a binned histogram showing count of reviews
+    ranging from negative to positive sentiments score.
+
+    Parameters
+    ----------
+    df : Pandas DataFrame
+        DataFrame containing the reviews
+    col: String
+        Column name in the dataframe that contains the reviews 
+
+    Returns
+    -------
+    Plot:
+        A plot object
+
+    Examples
+    --------
+    >>> plot = sentiment_score_plot(df, "reviews")
+    >>> plt.show()
+    """
+    pass
