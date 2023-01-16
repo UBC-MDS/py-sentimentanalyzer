82438f06a2127a2f63d1fe3240597fb871a3c836
Eric Tsai <eric.tsai1208@gmail.com>
2023-01-16 15:29:53 -0800

    implemented likert_scale function'

--- src/pysentimentanalyzer/likert_scale.py
+++ src/pysentimentanalyzer/likert_scale.py
@@ -1,10 +1,14 @@
-def convert_to_likert(df):
+from get_aggregated_sentiment_score import get_aggregated_sentiment_score
+
+def convert_to_likert(df, col):
     """Convert the sentiment scores to a likert scale from 1-7
     
     Parameters
     ----------
     df : Pandas DataFrame
         DataFrame containing the text to perform sentiment analysis
+    col : string
+        Name of the column of text in the Dataframe
 
     Returns
     -------
@@ -17,4 +21,17 @@ def convert_to_likert(df):
     --------
     >>> convert_to_likert(counts)
     """
-    pass
+    agg_score = get_aggregated_sentiment_score(df, col)
+    if agg_score >= -1 and agg_score < -0.6:
+        return "very negative", 1
+    elif agg_score >= -0.6 and agg_score < -0.2:
+        return "negative", 2
+    elif agg_score >= -0.2 and agg_score <= 0.2:
+        return "neutral", 3
+    elif agg_score > 0.2 and agg_score <= 0.6:
+        return "positive", 4
+    elif agg_score >0.6 and agg_score <= 1:
+        return "very positive", 5
+    else:
+        raise Exception("Aggregate compound score not in the valid range.")
+    
