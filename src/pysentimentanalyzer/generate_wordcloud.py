0ea75a93a337b439b438f7e4c59f610f48cf4c42
Ziyi Chen <chenziyi1997@live.cn>
2023-01-20 19:03:38 -0800

    Modified geerate_wordcloud function and added test function

--- src/pysentimentanalyzer/generate_wordcloud.py
+++ src/pysentimentanalyzer/generate_wordcloud.py
@@ -1,4 +1,7 @@
-def generate_wordcloud(df):
+from wordcloud import WordCloud
+from pysentimentanalyzer.helper import get_sentiment_and_score
+
+def generate_wordcloud(df, col):
     """
     Generate the word cloud of the most common positive and negative words from a given survey in the form of a 
     data frame and create a word cloud.
@@ -10,13 +13,39 @@ def generate_wordcloud(df):
 
     Returns
     -------
-    WordCloud:
-        A word cloud instance that can be later plotted or saved as an image
+    List:
+         A list of three images in the following order: postive, neutral, negative
+         A word cloud instance that can be later plotted or saved as an image
 
     Examples
     --------
-    >>> wc = generate_wordcloud(df)
-    >>> plt.imshow(wc, interpolation="bilinear")
-    >>> plt.show()
+     >>> imgs= generate_wordcloud(df)
+     >>> imgs[0].show() # positive
+     >>> imgs[1].show() # negative
+     >>> imgs[2].show() # neutral
     """
-    pass
+    if df is None:
+        raise TypeError("df must not be None")
+    
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("df must be a DataFrame")
+    
+    if col is None:
+        raise TypeError("col must not be None") 
+
+    df =  get_sentiment_and_score(df,col) 
+    neg_df = df[df["sentiment"] == "negative"]
+    pos_df = df[df["sentiment"] == "positive"]
+    neutral_df = df[df["sentiment"] == "neutral"]
+
+    negative_messages = " ".join(neg_df[col].tolist())
+    positive_messages = " ".join(pos_df[col].tolist())
+    neutral_messages = " ".join(neutral_df[col].tolist())
+
+
+    wordcloud = WordCloud(max_font_size=40, width=800, height=400)
+    pos_image = wordcloud.generate(negative_messages).to_image()
+    neg_image = wordcloud.generate(positive_messages).to_image()
+    neutral_image = wordcloud.generate(neutral_messages).to_image()
+
+    return [pos_image,neutral_image, neg_image]
